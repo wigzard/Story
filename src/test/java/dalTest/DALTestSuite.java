41e9738b27f6f24a5ce3f0f79fd5c23e7527b093
wigzard <nedolugin@gmail.com>
2016-08-25 14:48:01 +0300

    Test RetriveMapsAction

--- src/test/java/dalTest/DALTestSuite.java
+++ src/test/java/dalTest/DALTestSuite.java
@@ -1,5 +1,6 @@
 package dalTest;
 
+import dalTest.DataActionsTest.RetrieveMapsActionTest;
 import dalTest.loadSettings.SettingsLoaderTest;
 import org.junit.runner.RunWith;
 
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 @RunWith(org.junit.runners.Suite.class)
 @org.junit.runners.Suite.SuiteClasses({
         SettingsLoaderTest.class,
+        RetrieveMapsActionTest.class,
 })
 public class DALTestSuite {
 }
